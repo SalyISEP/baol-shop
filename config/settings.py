0f1070b1ab0654151d71758cdba8d11193df3ca4
SalyISEP <salyd242@gmail.com>
2026-09-12 16:03:08 +0000

    Fix Render host configuration

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS',
-    'localhost,127.0.0.1'
-).split(',')
+ALLOWED_HOSTS = [
+    'baol-shop.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://baol-shop.onrender.com',
