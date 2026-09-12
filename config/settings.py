42145a6fc44ca1f5db03c28b1bd28848a4ac820e
SalyISEP <salyd242@gmail.com>
2026-09-12 16:36:30 +0000

    Test Render host

--- config/settings.py
+++ config/settings.py
@@ -27,11 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'baol-shop.onrender.com',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
+
 print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
 CSRF_TRUSTED_ORIGINS = [
     'https://baol-shop.onrender.com',
